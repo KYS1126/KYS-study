f70f38dacd025edf24bd946b3065295d8ce461ef
github KYS1126 <github kimyongsu1226@naver.com>
2022-10-30 21:35:01 +0900

    up

--- study/쉑쉑버거/css/swiper.js
+++ study/쉑쉑버거/css/swiper.js
@@ -1,5 +1,69 @@
 var swiper = new Swiper(".mySwiper", {
   pagination: {
     el: ".swiper-pagination",
+    type: 'bullets'
   },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay:3000,
+  },
+  speed: 800,
+  loop:true,
 });
+
+
+var swiper1 = new Swiper(".mySwiper1", {
+  loop:true,
+  slidesPerView: 4,
+  spaceBetween: 30,
+  centeredSlides: true,
+  pagination: {
+    // el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay:1000,
+  },
+  speed: 2000,
+});
+
+var swiper2 = new Swiper(".mySwiper2", {
+  loop:true,
+  slidesPerView: 4,
+  spaceBetween: 30,
+  centeredSlides: true,
+  pagination: {
+    // el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay:1000,
+  },
+  speed: 2000,
+});
+
+var swiper3 = new Swiper(".mySwiper3", {
+  loop:true,
+  slidesPerView: 4,
+  spaceBetween: 30,
+  centeredSlides: true,
+  pagination: {
+    // el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay:1000,
+  },
+  speed: 2000,
+});
+
+
+
+const button1 = document.querySelector('button1')
+
+button1.addEventListener('click', function(){
+  style
+})
